90f84a88f92fe89f768d7588d5417c60190561a5
Venkata Durga Namdve <venkatadurganamdev@gmail.com>
2025-08-27 17:14:39 +0530

    helloworld

--- Conservation.sh
+++ Conservation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "$Trumph:: Hey Musk How are you?"
-echo "$Musk:: I am fine Trumph"
-echo "$Trumph:: Hey Mush What are you doing"
-echo "$Musk:: I am Planning to go Mars for Some Research could you come"
-echo "$Trumph:: No, First you go and come back then i will come"
+echo "Trumph:: Hey Musk How are you?"
+echo "Musk:: I am fine Trumph"
+echo "Trumph:: Hey Mush What are you doing"
+echo "Musk:: I am Planning to go Mars for Some Research could you come"
+echo "Trumph:: No, First you go and come back then i will come"
